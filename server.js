8921e3a44bc3981ba52cff510baa23d554234bde
SubodhKBansal <bansalsubodh1004@gmail.com>
2026-07-23 14:36:37 +0530

    83rd commit

--- server.js
+++ server.js
@@ -83,8 +83,8 @@ app.post("/register", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+console.log("PORT =", process.env.PORT);
 
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
