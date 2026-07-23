5931408dfcec97fe596849436ee3f50147c11c18
SubodhKBansal <bansalsubodh1004@gmail.com>
2026-07-23 14:01:00 +0530

    82nd commit

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static("public"));
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 // ==============================
 // Test PostgreSQL Connection
 // ==============================
